4b9a5d6a39bd0824755bd292379b4c6104503df2
Uwem Uke <slyart147@gmail.com>
2020-03-07 02:11:01 +0100

    snapshot test

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
-//import Bookings from './components/Bookings';
-//import Meals from './components/Meals';
-//import Error from './components/Error';
 import Guest from './components/Guest';
 
 
-
-/*class App extends Component {
-    constructor(props){
-        super(props);
-        this.setGuest = this.setGuest.bind(this);
-        this.state ={
-            guests: ''
-
-        }
-    }
-    
-    setGuest(obj){
-        this.setState({guests: obj});
-    }
-    
-
-    render() {
-        return (
-            <div className="container-fluid">
-                <center>
-                    <h2>Hacker Hostel</h2>
-                </center>
-                <div className="container">
-                    <Bookings setGuest={this.setGuest} />
-                    <Error guests={this.state.guests} />
-                    <Meals guests={this.state.guests} />
-                </div>
-            </div>
-        );
-    }
-}
-
-export default App;*/
 
 
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import Guest from './components/Guest';
 
 it('renders without crashing', () => {
   shallow(<App />);
@@ -12,4 +14,39 @@ test('renders Guests', () => {
   const { getByText } = render(<App />);
   const guest = getByText(/Guests/i);
   expect(guest).toBeInTheDocument();
+});
+
+it('finds submit button', () => {
+  	let wrapper = mount(<App />);
+  	expect(wrapper.find('button').text()).toEqual('Send Guest');
+});
+
+it('shows input text', () => {
+  	let wrapper = mount(<App />);
+  	wrapper.find('input').simulate('change', {
+		target: {value: 'foo'}
+	});
+	wrapper.find('button').simulate('click');
+	expect(wrapper.containsMatchingElement(<p>My new guest is foo</p>)).toBeTruthy()
+});
+
+
+it('takes snapshot of state changes', () => {
+	const component = renderer.create(
+		<App />
+	);	
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+
+	//input and clicked
+	let instance = component.root;
+
+	let findInput = instance.findByType('input');
+	findInput.props.onChange({ target: { value: 'bar' } });
+
+	let findButton = instance.findByType('button');
+	findButton.props.onClick();
+	
+	tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
 });
